c8739d7588460dbb7bcb392600e94ba3f08a4997
Enam Rahman Eraz <92011676+wo0z1e@users.noreply.github.com>
2024-11-24 23:46:39 +0600

    Update name.php

--- lab4/name.php
+++ lab4/name.php
@@ -1,26 +1,30 @@
 <?php
-if(isset($_REQUEST["submit"])) {   
+if (isset($_REQUEST["submit"])) {
 
-$name = $_REQUEST["name"];
+    $name = $_REQUEST["name"];
 
-if($name== null || empty($name)){
-    echo"Name cannot be empty!!";
-}elseif(str_word_count($name)< 2){
- echo "Name should at least be 2 words !!";
- }
- 
- elseif(ctype_alpha($name[0]) ){
-    echo "Your Name is ".$name ;
- }
- else{
-    echo "Invalid!! <br>
-    A. Cannot be empty <br>
-B. Contains at least two words <br>
-C. Must start with a letter <br>
-D. Can contain a-z, A-Z, period, dash only";
- }
-}
-else{
+    if (empty($name)) {
+        echo "Name cannot be empty!!";
+    } elseif (str_word_count($name) < 2) {
+        echo "Name should at least be 2 words !!";
+    } elseif (!ctype_alpha($name[0])) {
+        echo "Your Name must start with Upper or lower class Alphabet !!";
+    } else {
+        for ($i = 0; $i < strlen($name); $i++) {
+            $char = $name[$i];
+            $isvalid=false;
+            if (ctype_alpha($name) || $char == "." || $char == "-" || $char == " ") {
+                $isvalid = true;
+            }
+        }
+        if($isvalid){
+            
+            echo "Your Name is " .$name;
+        }
+        else{
+            echo"Name can contain a-z, A-Z, period, dash only";
+        }
+    }
+} else {
     header("location:name.html");
 }
-?>
